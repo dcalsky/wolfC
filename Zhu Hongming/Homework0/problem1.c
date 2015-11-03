882e985a7010f9dee9ddcc04456fca32b90e7242
ecnelises <fwage73@gmail.com>
2015-11-03 21:37:42 +0800

    增加了Zhu Hongming目录的代码

---
+++ Zhu Hongming/Homework0/problem1.c
@@ -0,0 +1,12 @@
+/* just for fun
+ * can you understand it?
+ * :)
+ */
+#include <stdio.h>
+#define _O_O_(g) (0)
+#define _P_P_(o, u) ((!_O_O_(u))?(pr##o##f(u))-1:(0))
+#define _Q_Q_(n_n,m_m) n_n m_m
+#define _T_T_(n) #n"\x0a"
+#define _M_M_(t) #t
+#define TAT r##etu##rn (_P_P_(int, _Q_Q_(_T_T_(hello),_M_M_(c language))));
+int main(){TAT}
